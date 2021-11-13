81c25a89dda456c34cd0ae7554c409654a34b2ff
Johnny Magrippis <j@magrippis.com>
2021-11-13 16:56:25 +0000

    ✨ add burger menu

---
+++ src/lib/Hamburger.svelte
@@ -0,0 +1,75 @@
+<script>
+  export let open = false
+  export let onClick = () => {
+    open = !open
+  }
+  export let width = 80
+  export let ariaLabel = 'menu'
+</script>
+
+<button on:click={onClick} aria-expanded={open} aria-label={ariaLabel}>
+  <svg
+    class:open
+    viewBox="0 0 100 100"
+    {width}
+    fill="none"
+    stroke="currentColor"
+    stroke-width="5"
+  >
+    <path
+      class="top"
+      d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+    />
+    <path class="middle" d="m 30,50 h 40" />
+    <path
+      class="bottom"
+      d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+    />
+  </svg>
+</button>
+
+<style>
+  :root {
+    --transition-duration: 400ms;
+  }
+
+  button {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    transition: transform var(--transition-duration);
+  }
+
+  .open {
+    transform: rotate(45deg);
+  }
+
+  .top {
+    stroke-dasharray: 40 160;
+    transition: stroke-dashoffset var(--transition-duration);
+  }
+
+  .middle {
+    transform-origin: 50%;
+    transition: transform var(--transition-duration);
+  }
+  .bottom {
+    stroke-dasharray: 40 85;
+    transition: stroke-dashoffset var(--transition-duration);
+  }
+
+  .open .top,
+  .open .bottom {
+    stroke-dashoffset: -64px;
+  }
+
+  .open .middle {
+    transform: rotate(90deg);
+  }
+  .open .bottom {
+    stroke-dashoffset: -64px;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,26 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+  export const prerender = true
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+  import Hamburger from '$lib/Hamburger.svelte'
 </script>
 
 <svelte:head>
-	<title>Home</title>
+  <title>Hamburger</title>
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+  <Hamburger width={500} />
+  <slot />
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
 </style>
