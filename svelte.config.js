6e0e03bf6248a2d94b0628236ec55ec6d176a11a
Johnny Magrippis <j@magrippis.com>
2021-11-14 09:53:29 +0000

    🚀  set up adapter for deployment to Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: vercel({ out: 'my-output-directory' }),
+  },
+}
 
-export default config;
+export default config
